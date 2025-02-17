cd8651ad024532c492834388dff562823e243341
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-02-17 19:39:41 +0100

    HomePage + styles

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,46 @@
 // src/pages/HomePage/HomePage.tsx
 
+import { useNavigate } from 'react-router-dom';
+import styles from './HomePage.module.css';
+import '../../index.css';
+
 export const HomePage: React.FC = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  // const registeredUsers = "";
+
+  return (
+    <div className={`${styles.homeSection} section`}>
+      <div className={`${styles.homeContainer} container`}>
+        <div className={styles.firstSide}>
+          <blockquote className={styles.quoteBox}>
+            <span className={styles.textBlack}>THE SECRET OF </span>
+            <span className={styles.textUnderline}>GETTING AHEAD </span>
+            <span className={styles.textBlack}>IS GETTING </span>
+            <span className={styles.textUnderlineLarge}>STARTED</span>
+          </blockquote>
+        </div>
+
+          <div className={styles.secondSide}>
+            <h1 className={styles.title}>Road to the Dream 71</h1>
+            <p className={styles.subtitle}>
+              Transform your life in 71 days through discipline and dedication
+            </p>
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.buttonLogIn}
+                onClick={() => navigate('/login')}
+              >
+                I already have an account
+              </button>
+              <button
+                className={styles.buttonSignUp}
+                onClick={() => navigate('/signup')}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 };
