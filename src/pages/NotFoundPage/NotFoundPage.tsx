c1d44a2f71252c1111b5219296f672d9285ee146
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-02-17 20:07:59 +0100

    NotFound Page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,31 @@
 // src/pages/NotFoundPage/NotFoundPage.tsx
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './NotFoundPage.module.css';
+import '../../index.css';
 
 export const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for doesn't exist.</p>
+    <div className={`${styles.notFoundSection} section`}>
+      <div className={`${styles.notFoundContainer} container`}>
+        <div className={styles.messageContainer}>
+          <h1 className={styles.title}>404 - Page Not Found</h1>
+          <p className={styles.subtitle}>
+            The page you are looking for doesn't exist.
+          </p>
+          <div className={styles.buttonContainer}>
+            <button
+              className={styles.buttonHome}
+              onClick={() => navigate('/')}
+            >
+              Go to Home Page
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
