e29e5f5ada5c98f0ab2efa2806c1d23bce7a65a7
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-02-22 19:34:38 +0100

    SignUp Page

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,62 +1,150 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import styles from './SignUpForm.module.css';
-import { LogInButton } from '../LogInButton/LogInButton';
+import { LogInButtonTransparent } from '../LogInButtonTransparent/LogInButtonTransparent';
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .trim()
+    .required('Email is required')
+    .email('Invalid email format'),
+  password: yup
+    .string()
+    .trim()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters long'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), undefined], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
 
 export const SignUpForm: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(
-      'Email:',
-      email,
-      'Password:',
-      password,
-      'Confirm Password:',
-      confirmPassword
-    );
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  interface FormData {
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
+
+  const onSubmit = async (data: FormData) => {
+    const { email, password } = data;
+    console.log('Email:', email, 'Password:', password);
+    reset();
   };
 
   return (
-    <form className={styles.signUpForm} onSubmit={handleSubmit}>
-      <div className={styles.formGroup}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
+    <div className={styles.infoContainer}>
+      <div className={styles.formContainer}>
+        <form
+          className={styles.form}
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h1 className={styles.title}>Sign-Up</h1>
+          <div className={styles.inputBox}>
+            <div className={styles.group}>
+              <label htmlFor="email" className={styles.label}>
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className={`${styles.input} ${
+                  errors.email ? styles.error : ''
+                }`}
+                {...register('email')}
+                autoComplete="email"
+              />
+              {errors.email && (
+                <p className={styles.error}>{errors.email.message}</p>
+              )}
+            </div>
+
+            <div className={styles.group}>
+              <label htmlFor="password" className={styles.label}>
+                Password
+              </label>
+              <div className={styles.wrapPass}>
+                <input
+                  type={passwordVisible ? 'text' : 'password'}
+                  id="password"
+                  placeholder="Enter your password"
+                  className={`${styles.input} ${
+                    errors.password ? styles.error : ''
+                  }`}
+                  {...register('password')}
+                  autoComplete="current-password"
+                />
+                <button
+                  type="button"
+                  className={styles.toggle}
+                  onClick={() => setPasswordVisible(!passwordVisible)}
+                >
+                  {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+                </button>
+                {errors.password && (
+                  <p className={styles.error}>{errors.password.message}</p>
+                )}
+              </div>
+            </div>
+
+            <div className={styles.group}>
+              <label htmlFor="confirmPassword" className={styles.label}>
+                Confirm Password
+              </label>
+              <div className={styles.wrapPass}>
+                <input
+                  type={confirmPasswordVisible ? 'text' : 'password'}
+                  id="confirmPassword"
+                  placeholder="Confirm your password"
+                  className={`${styles.input} ${
+                    errors.confirmPassword ? styles.error : ''
+                  }`}
+                  {...register('confirmPassword')}
+                  autoComplete="new-password"
+                />
+                <button
+                  type="button"
+                  className={styles.toggle}
+                  onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
+                >
+                  {confirmPasswordVisible ? <FaEye /> : <FaEyeSlash />}
+                </button>
+                {errors.confirmPassword && (
+                  <p className={styles.error}>{errors.confirmPassword.message}</p>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <button type="submit" className={styles.signUpBtn}>
+            Sign Up
+          </button>
+
+          <div className={styles.signUpOffer}>
+            <p className={styles.signUpOfferText}>Do you already have an account?</p>
+              <LogInButtonTransparent />
+          </div>
+        </form>
       </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          value={confirmPassword}
-          onChange={e => setConfirmPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit" className={styles.submitButton}>
-        Sign Up
-      </button>
-      <LogInButton />
-    </form>
+    </div>
   );
 };
-
-export default SignUpForm;
